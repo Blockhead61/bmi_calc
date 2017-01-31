c22bd22c3328d97d6d0f6ddefc7e2a3cbf09d3f0
Blockhead61 <matcrampton@gmail.com>
2017-01-31 14:55:15 +0000

    Update bmi-opt1.js

--- assets/js/bmi-opt1.js
+++ assets/js/bmi-opt1.js
@@ -11,13 +11,13 @@ var metrichunit = document.getElementById( 'hi' ),
 	metricwunit = document.getElementById( 'wi' ),
 	metrichunitsml = document.getElementById( 'thi' );
 	metricwunitsml = document.getElementById( 'twl' );
-	;
 
 window.onload = function() {
 metrichunit.style.display = 'none';
 metricwunit.style.display = 'none';
 metrichunitsml.style.display  = 'none';
 metricwunitsml.style.display  = 'none';
+document.getElementById("msm1").checked = true;
 }
 function msystem() {
 	
@@ -61,22 +61,20 @@ if((ms==='metric'&&height>0) || (ms==='us'&&height>0)) {
 var heighttotalimperial = height*12 + +heightunit,
 	weighttotalimperial = weight*14 + +weightunit;
 	heightmetric = height*100;
-	//heighttotal = height*100 + +heightunit,
 	
 if(ms==='metric'&&height>0){
-	//$('bmi').textContent=Math.round(weighttotal/(heighttotal*heighttotal/10000)*100)/100 ;  
 	$('bmi').textContent=Math.round(weight/(heightmetric*heightmetric/10000)*100)/100 ;  
 }else if(ms==='us'&&height>0){
 	$('bmi').textContent=Math.round(703*weighttotalimperial/(heighttotalimperial*heighttotalimperial)*100)/100;
 }
 if ($('bmi').textContent < 18.5) {
-	you = 'You are below the recommended weight for your height and should not be attempting to lose weight. A healthy BMI range is between 18.5-25. We recommend that you consult your GP.</p>';
+	you = 'You are below the recommended weight for your height and should not be attempting to lose weight. A healthy BMI range is between 18.5-25. We recommend that you consult your GP.</p><p class="bold">A healthy BMI range is between 18.5-25</p>';
 } else if ($('bmi').textContent > 18.5 && ($('bmi').textContent < 24.9)) {
 	you = 'You are a normal weight range for your height. However it is still important to eat healthily.';
 } else if ($('bmi').textContent > 24.9 && ($('bmi').textContent < 29.9)) {
-	you = 'You are within the overweight range and your health will benefit if you lost a little weight. You certaintly don\'t want to put on any weight. Look at the BMI chart to see your ideal weight range for your height.';
+	you = 'You are within the overweight range and your health will benefit if you lost a little weight. You certaintly don\'t want to put on any weight. Look at the BMI chart to see your ideal weight range for your height.<p class="bold">A healthy BMI range is between 18.5-25</p>';
 } else {
-	you = 'You are classified as obese and your health will greatly benefit if you were able to lose weight. Being obese means that you are at increased risk of diet related diseases such as diabetes and heart disease. Look at the BMI chart to see your ideal weight range for your height. We recommend that you visit your GP to discuss losing weight safely through getting more active and a health balanced diet.';
+	you = 'You are classified as obese and your health will greatly benefit if you were able to lose weight. Being obese means that you are at increased risk of diet related diseases such as diabetes and heart disease. Look at the BMI chart to see your ideal weight range for your height. We recommend that you visit your GP to discuss losing weight safely through getting more active and a health balanced diet.<p class="bold">A healthy BMI range is between 18.5-25</p>';
 }
 youAre.innerHTML = you;
 }
